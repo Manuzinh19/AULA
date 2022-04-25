3717df665c31b9dad3c7b2c54c10a7896b87ae65
Manuzinh19 <104384332+Manuzinh19@users.noreply.github.com>
2022-04-25 14:41:46 -0300

    Add files via upload

---
+++ aula/aula/src/pac2/Main.java
@@ -0,0 +1,20 @@
+package pac2;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String file = "arquivo.txt";
+        try (Stream<String> stream = Files.lines(Paths.get(file))) {
+            stream.forEach(System.out::println);
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+    
+
